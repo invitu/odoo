10e2f427ab9d8513ca94fb65f6345007c5c10113
Cyril VINH-TUNG <cyril@invitu.com>
2023-12-27 16:37:46 -1000

    [ADD] Add modules + licence + data

---
+++ pharmapf_profile/__manifest__.py
@@ -0,0 +1,33 @@
+{
+    'name': 'Pharma PF Profile',
+    'version': '1.0',
+    'summary': 'Verticalisation for Pharmacies in PF',
+    'sequence': 30,
+    'description': """
+""",
+    'category': 'Customization',
+    "author": "INVITU SARL",
+    'website': 'https://www.invitu.com',
+    'images': [
+    ],
+    'depends': [
+        'base',
+        'pos_partner_birthdate',
+        'pos_order_split_invoice',
+        'pos_multi_order_payment',
+    ],
+    'data': [
+        'data/res_config_settings.xml',
+        'data/res_partner.xml',
+        'data/product_category.xml',
+        'data/product_pricelist.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'licence': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
